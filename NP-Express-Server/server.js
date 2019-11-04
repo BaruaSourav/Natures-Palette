80c117a6e191e23977ac66be6a0f7f0523f21a46
Sourav Barua <sourav.shr6@hotmail.com>
2019-11-04 01:57:32 -0330

    Configured serverside application for DB and route.

---
+++ NP-Express-Server/server.js
@@ -0,0 +1,27 @@
+
+const express = require('express');
+path = require('path');
+bodyParser = require('body-parser');
+cors = require('cors');
+mongoose = require('mongoose');
+config = require('./DBConfiguration');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+    () => { console.log('Database is connected') },
+    err => { console.log('Can not connect to the database' + err) }
+);
+
+//express app configuarition
+const app = express();
+let port = process.env.PORT || 4000;
+
+
+//Routes
+app.get('/', function (req, res) {
+    res.send('Testing Routes.')
+})
+
+const server = app.listen(port,function () {
+    console.log('NPServer Listening on port ' + port);
+});
