03e9171ce37a436a24e536fea03ac9c3f2be557d
Sourav Barua <sourav.shr6@hotmail.com>
2019-11-28 18:01:29 -0330

    fixed the unzipping after submission

--- NP-Express-Server/Routes/submission.route.js
+++ NP-Express-Server/Routes/submission.route.js
@@ -1,71 +1,87 @@
 // Submission.route.js
-const express = require('express');
+const express = require("express");
 const app = express();
 const submissionroutes = express.Router();
-var multer = require('multer');
+var multer = require("multer");
 const fs = require("fs");
 const path = require("path");
 const csv = require("csv-parser");
+var AdmZip = require("adm-zip");
 
 // Submission Model
-let SubmissionModel = require('../Models/SubmissionModel');
-let MetadataInfoModel = require('../Models/MetadataInformationModel');
+let SubmissionModel = require("../Models/SubmissionModel");
+let MetadataInfoModel = require("../Models/MetadataInformationModel");
 
-var upload = multer()
+var upload = multer();
 // Defined store route for submission information
-submissionroutes.route('/add').post(upload.none(), function (req, res) {
+submissionroutes.route("/add").post(upload.none(), function(req, res) {
   // moving metadata files from temp to metadatafiles folder
   let tempmetadatapath = path.resolve(
-        __dirname,
-        "../filepersistance/tempvalidationfiles/" + req.body.MetaDataFileName
-      );
-  let metadatapath =  path.resolve(
-        __dirname,
-        "../filepersistance/metadatafiles/" + req.body.MetaDataFileName
-      );
-  let moveMetadataFileCallback = function(e){
+    __dirname,
+    "../filepersistance/tempvalidationfiles/" + req.body.MetaDataFileName
+  );
+  let metadatapath = path.resolve(
+    __dirname,
+    "../filepersistance/metadatafiles/" + req.body.MetaDataFileName
+  );
+  let moveMetadataFileCallback = function(e) {
     console.log(e);
-  }
+  };
   fs.rename(tempmetadatapath, metadatapath, moveMetadataFileCallback);
   ///////////////////////////////////////////////////////////////////////////
-   // moving rawfile from temp to metadatafiles folder
+  // moving rawfile from temp to metadatafiles folder
   let temprawfilepath = path.resolve(
-        __dirname,
-        "../filepersistance/tempvalidationfiles/" + req.body.RawFileName
-      );
-  let rawfilepath =  path.resolve(
-        __dirname,
-        "../filepersistance/rawfiles/" + req.body.RawFileName
-      );
-  let moveRawFileCallback = function(e){
-    console.log(e);
-  }
+    __dirname,
+    "../filepersistance/tempvalidationfiles/" + req.body.RawFileName
+  );
+  let rawfilepath = path.resolve(
+    __dirname,
+    "../filepersistance/rawfiles/" + req.body.RawFileName
+  );
+  let moveRawFileCallback = function(e) {
+    let rawFileDirectory = path.resolve(
+      __dirname,
+      "../filepersistance/rawfiles/"
+    );
+    var zippedRawFile = new AdmZip(rawfilepath);
+    var zipEntries = zippedRawFile.getEntries();
+    zipEntries.shift();
+    zipEntries.forEach(entry => {
+      zippedRawFile.extractEntryTo(entry.entryName, rawFileDirectory, false, true);
+    });
+    fs.unlink(rawfilepath);
+    //zippedRawFile.extractAllTo(rawFileDirectory,true);
+
+  };
   fs.rename(temprawfilepath, rawfilepath, moveRawFileCallback);
   // TODO: Need to unzip the raw file here
-  //  Reading metadata csv file's rows
-  fs.createReadStream(metadatapath).pipe(csv())
-  .on("data", row => {
-    let metadatainfo = new MetadataInfoModel(row);
-    //console.log(metadatainfo);
-    metadatainfo.save()
-      .then(metadatainfo => {
-        //console.log(metadatainfo);
-      })
-      .catch(err =>{
-        console.log(err);
-      });
-  });
 
+  
+
+  //  Reading metadata csv file's rows
+  fs.createReadStream(metadatapath)
+    .pipe(csv())
+    .on("data", row => {
+      let metadatainfo = new MetadataInfoModel(row);
+      //console.log(metadatainfo);
+      metadatainfo
+        .save()
+        .then(metadatainfo => {
+          //console.log(metadatainfo);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    });
 
   let submission = new SubmissionModel(req.body);
-  submission.save()
+  submission
+    .save()
     .then(submission => {
-      res.status(200).json(
-        {
-          'Success': 'The submission stored successfully',
-          'body': req.body
-        }
-      );
+      res.status(200).json({
+        Success: "The submission stored successfully",
+        body: req.body
+      });
     })
     .catch(err => {
       res.status(400).send("Unable to save to database");
@@ -73,12 +89,11 @@ submissionroutes.route('/add').post(upload.none(), function (req, res) {
 });
 
 // Defined get data(index or listing) route
-submissionroutes.route('/').get(function (req, res) {
-  SubmissionModel.find(function (err, submissions) {
+submissionroutes.route("/").get(function(req, res) {
+  SubmissionModel.find(function(err, submissions) {
     if (err) {
       console.log(err);
-    }
-    else {
+    } else {
       res.json(submissions);
     }
   });
