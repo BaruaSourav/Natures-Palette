811f5f151c1ccf071db7f97b03267165e726acde
Sourav Barua <sourav.shr6@hotmail.com>
2019-11-28 21:18:38 -0330

    Working on download function.

--- NP-Frontends/NaturesPaletteUI/src/app/NaturesPaletteConfig.ts
+++ NP-Frontends/NaturesPaletteUI/src/app/NaturesPaletteConfig.ts
@@ -2,5 +2,7 @@ export class NaturesPaletteConfig{
   public apiurl: string;
   constructor() {
     this.apiurl = 'http://localhost:4000';
+    //this.apiurl = 'http://10.20.250.157:4000;
+    //this.apiurl = 'http://192.168.2.149:4000'
   }
 }
