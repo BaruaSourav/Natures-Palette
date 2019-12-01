675a0abc71ab15e42b0cf8b2a4d2a8454a0da1a7
Sourav Barua <sourav.shr6@hotmail.com>
2019-12-01 19:51:55 -0330

    Config Change

--- NP-Frontends/NaturesPaletteUI/src/app/NaturesPaletteConfig.ts
+++ NP-Frontends/NaturesPaletteUI/src/app/NaturesPaletteConfig.ts
@@ -1,8 +1,9 @@
 export class NaturesPaletteConfig{
   public apiurl: string;
   constructor() {
-    //this.apiurl = 'http://localhost:4000';
-    this.apiurl = 'http://10.20.250.157:4000';
+    this.apiurl = 'http://ec2-3-85-164-248.compute-1.amazonaws.com:4000';
+    //this.apiurl = 'http://10.20.250.157:4000';
     //this.apiurl = 'http://192.168.2.149:4000'
+    //this.apiurl = 'http://localhost:4000'
   }
 }
